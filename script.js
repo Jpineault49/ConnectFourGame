8e815d58ab0a9ca09533bc1834549f17e42fe0cb
Jpineault49 <157521242+Jpineault49@users.noreply.github.com>
2024-01-25 01:10:49 +0000

    Showing who wins should work better now as well proper winning checking.

--- script.js
+++ script.js
@@ -37,40 +37,38 @@ function new_game() {
   player_two = prompt("Player 2 Name (YELLOW):")
 
   $("game_board").hidden = false
-  $("whos_turn").hidden = false
   turn_indicator()
+
 }
 
 function turn_indicator() {
-  let output = document.getElementById("whos_turn");
-
+  let output = document.getElementById("who's_turn");
+  output.hidden = false 
   if (turn == 1) {
     output.innerText = "Player One's Turn! (RED)";
   } else if (turn == 2) {
-    output.innerText = "Player Two's Turn! (YELLOW)";
+    output.innerText = "Player Two's Turn!(YELLOW)";
   }
 }
 
-
 function winner() {
   //Check Horizontal
-  for (let row = 0; row < 5; row++) {
+  //Check if piece is red (1)
+  for (let row = 0; row < 6; row++) {
     for (let col = 0; col < 4; col++) {
-
-      if (grid[row][col] !== 0 &&
+      if 
+        (grid[row][col] === 1 &&
         grid[row][col] === grid[row][col + 1] &&
         grid[row][col] === grid[row][col + 2] &&
         grid[row][col] === grid[row][col + 3]) {
         return true;
       }
-
     }
   }
-
   //Check Vertical 
   for (let row = 0; row < 3; row++) {
     for (let col = 0; col < 7; col++) {
-      if (grid[row][col] !== 0 &&
+      if (grid[row][col] === 1 &&
         grid[row][col] === grid[row + 1][col] &&
         grid[row][col] === grid[row + 1][col] &&
         grid[row][col] === grid[row + 3][col]) {
@@ -78,11 +76,10 @@ function winner() {
       }
     }
   }
-
   //Check Diagnal
   for (let row = 0; row < 3; row++) {
     for (let col = 0; col < 4; col++) {
-      if (grid[row][col] !== 0 &&
+      if (grid[row][col] === 1 &&
         grid[row][col] === grid[row + 1][col + 1] &&
         grid[row][col] === grid[row + 2][col + 2] &&
         grid[row][col] === grid[row + 3][col + 3]) {
@@ -90,20 +87,59 @@ function winner() {
       }
     }
   }
-
   for (let row = 0; row < 3; row++) {
     for (let col = 0; col < 7; col++) {
-      if (grid[row][col] !== 0 &&
+      if (grid[row][col] === 1 &&
         grid[row][col] === grid[row + 1][col - 1] &&
         grid[row][col] === grid[row + 1][col - 1] &&
         grid[row][col] === grid[row + 3][col - 3]) {
         return true;
-
       }
     }
   }
-  return false;
 
+  //Check if piece is yellow(2)
+  for (let row = 0; row < 6; row++) {
+    for (let col = 0; col < 4; col++) {
+      if (grid[row][col] === 2 &&
+        grid[row][col] === grid[row][col + 1] &&
+        grid[row][col] === grid[row][col + 2] &&
+        grid[row][col] === grid[row][col + 3]) {
+        return false;
+      }
+    }
+  }
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 7; col++) {
+      if (grid[row][col] === 2 &&
+        grid[row][col] === grid[row + 1][col] &&
+        grid[row][col] === grid[row + 1][col] &&
+        grid[row][col] === grid[row + 3][col]) {
+        return false;
+      }
+    }
+  }
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 4; col++) {
+      if (grid[row][col] === 2 &&
+        grid[row][col] === grid[row + 1][col + 1] &&
+        grid[row][col] === grid[row + 2][col + 2] &&
+        grid[row][col] === grid[row + 3][col + 3]) {
+        return false;
+      }
+    }
+  }
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 7; col++) {
+      if (grid[row][col] === 2 &&
+        grid[row][col] === grid[row + 1][col - 1] &&
+        grid[row][col] === grid[row + 1][col - 1] &&
+        grid[row][col] === grid[row + 3][col - 3]) {
+        return false;
+      }
+    }
+  }
+  return 0;
 }
 
 function piece_placement(event) {
@@ -114,18 +150,21 @@ function piece_placement(event) {
   let img_src; //Used to store URL of image 
 
 
-  if (winner()) {
-    return $("display_winner").innerText = "Player has won!";
-  } else if (winner() != true || false) {
-    console.log("Not a winning move!")
+  if (winner() === true) {
+    document.getElementById("win_display").hidden = false
+    return $("win_display").innerText = "Player 1 has won!";
+  } else if (winner() === false) {
+    document.getElementById("win_display").hidden = false
+    return $("win_display").innerText = "Player 2 has won";
+    
   } else {
-    return $("display_winner").innerText = "Its a Tie!";
+    console.log("Not a winning move!")
   }
+
 
   while (row >= 0) {
     if (grid[row][column_picked] === 0) { //check to see if the place is blank 
       id = `${row}${column_picked}`;  //id is the string that represents the HTML element        
-
       img_src = $(id).src = "images/blankcircle.png" // make variable that stores id of choosen spot with URL 
       break;
     } else {
@@ -134,8 +173,6 @@ function piece_placement(event) {
 
   }
   if (img_src === "images/blankcircle.png") {
-
-
     if (turn == 1) { //Player 1 goes first
       $(id).src = "images/red.png"
       grid[row][column_picked] = 1 //Make the blank a 1 / taken spot 
@@ -149,6 +186,8 @@ function piece_placement(event) {
   }
 
   if (row < 0) {
-    return "Column is full!";
+    $("who's_turn").innerText = "column is full!";
+  } else if (grid[row][column_picked] < 0) {
+    $("who's_turn").innerText = "Its a tie!";
   }
 }
